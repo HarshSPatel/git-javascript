860430034cc290584ab08c95e41ca4a24d74d325
Parth Raval <rava0023@algonquinlive.com>
2020-01-15 23:37:47 -0500

    Readme.md created & added the details as required

---
+++ README.md
@@ -0,0 +1,3 @@
+#Git Javascript
+
+##Harsh S. Patel
---
+++ script.js
@@ -0,0 +1,10 @@
+let name = 'Harsh'
+
+console.log(`Hello My Name is ${name},`)
+
+let number = 10
+
+console.log(`${number} is multiplied by 2 is ${number * 2},`)
+
+
+
