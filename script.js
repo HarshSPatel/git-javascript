8aa864137af029e2d6f8cbe7f5e6a147a959b4a6
Parth Raval <rava0023@algonquinlive.com>
2020-01-15 23:57:36 -0500

    Created & added script.js with requirements

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 let name = 'Harsh'
 
-console.log(`Hello My Name is ${name},`)
+console.log(`Hello My Name is ${name}.`)
 
 let number = 10
 
-console.log(`${number} is multiplied by 2 is ${number * 2},`)
+console.log(`My Positive Integer is ${number}.`)
+
+console.log(`${number} multiplied by 2 is ${number * 2}.`)
+
+let result = number * 2
+
+console.log(`${result} multiplied by 5 is ${result * 5}.`)
+
+let result2 = result * 5
+
+console.log(`${result2} divided by ${number} is ${result2 / number}.`)
+
+let finalresult = result2 / number
+
+console.log(`${finalresult} subracted by 7 is ${finalresul2 - 7}.`)
 
 
 
